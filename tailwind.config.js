aa6e76c8ce25e5ddb77db08f512bc8af0c603f08
Ejmul <mdejmulislam62@gmail.com>
2022-11-30 21:03:23 +0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container:{
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily: {
+        poppins:"'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+        'primary' : '#FD3D57'
+      }
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif']
+    }
+  },
+  variants:{
+    display: ['group-hover'],
+    visibility: ['group-hover'],
+  },
+  plugins: [require('@tailwindcss/forms'),],
+}
